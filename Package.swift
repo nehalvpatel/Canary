0cdcacc19988182c00d7aa0ed1f2ce4526b18fc3
Nehal Patel <nehalvpatels+github@gmail.com>
2019-10-26 05:25:55 +0100

    Fix dependency

--- Package.swift
+++ Package.swift
@@ -9,15 +9,17 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/yeokm1/SwiftSerial.git", from: "0.1.1"),
+        .package(url: "https://github.com/Flight-School/RegularExpressionDecoder.git", from: "0.1.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "Listener",
-            dependencies: ["SwiftSerial"]),
+            dependencies: ["RegularExpressionDecoder", "SwiftSerial"]),
         .testTarget(
             name: "ListenerTests",
             dependencies: ["Listener"]),
     ]
 )
+
