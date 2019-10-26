e82664ee6af3f3f641554152433aeba0d619a4dc
Nehal Patel <nehalvpatels+github@gmail.com>
2019-10-26 00:37:37 -0500

    Handle IFTTT network errors

--- Sources/Listener/PhoneSystem/Services/IFTTT.swift
+++ Sources/Listener/PhoneSystem/Services/IFTTT.swift
@@ -11,33 +11,37 @@ import FoundationNetworking
 #endif
 
 struct IFTTT: ServiceHandler {
-    struct Values {
+    struct Values : Codable {
         /// The dialer's Caller ID.
         let value1: String
         /// The dialed phone number that caused the action to trigger.
         let value2: PhoneNumber
         /// Not used yet.
-        let value3: String?
+        let value3: String
     }
     
     static func execute(_ action: Rule.Action, call: Call, caller: String, finish: @escaping (Result<Any, Error>) -> ()) {
-        let values = Values(value1: caller, value2: call.dialedNumber, value3: "")
-        
-        let url = URL(string: "https://maker.ifttt.com/trigger/\(action.event)/with/key/\(action.key)")!
-        var request = URLRequest(url: url)
-        request.httpMethod = "POST"
-        request.httpBody = try? JSONSerialization.data(withJSONObject: values)
-        request.setValue("application/json; charset=UTF-8", forHTTPHeaderField: "Content-Type")
-        
-        let task = URLSession.shared.dataTask(with: request) { data, response, error in
-            guard data != nil, error == nil else {
-                finish(.failure(error!))
-                return
+        do {
+            let values = Values(value1: caller, value2: call.dialedNumber, value3: "")
+            
+            let url = URL(string: "https://maker.ifttt.com/trigger/\(action.event)/with/key/\(action.key)")!
+            var request = URLRequest(url: url)
+            request.httpMethod = "POST"
+            request.httpBody = try JSONEncoder().encode(values)
+            request.setValue("application/json; charset=UTF-8", forHTTPHeaderField: "Content-Type")
+            
+            let task = URLSession.shared.dataTask(with: request) { data, response, error in
+                guard data != nil, error == nil else {
+                    finish(.failure(error!))
+                    return
+                }
+                
+                finish(.success(()))
             }
             
-            finish(.success(()))
+            task.resume()
+        } catch let error {
+            finish(.failure(error))
         }
-        
-        task.resume()
     }
 }
