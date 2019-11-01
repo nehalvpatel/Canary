2a425fb54624e644568c696c74ed44fe55bc75b8
Nehal Patel <nehalvpatels+github@gmail.com>
2019-10-31 23:14:20 -0500

    Documentation

--- Sources/Listener/ListenerCore/Rule.swift
+++ Sources/Listener/ListenerCore/Rule.swift
@@ -12,13 +12,17 @@ struct Rule : Codable {
         let phoneNumber: PhoneNumber
     }
     
-    enum Service: String, Codable {
-        case IFTTT
-    }
-    
     struct Action : Codable {
+        enum Service: String, Codable {
+            case IFTTT
+        }
+        
         let service: Service
+        
+        /// The API key to use for Webhook requests.
         let key: String
+        
+        /// The Webhook event to trigger.
         let event: String
     }
     
@@ -27,6 +31,10 @@ struct Rule : Codable {
 }
 
 extension Rule {
+    /// Perform all actions associated with the trigger number.
+    ///
+    /// - Parameter call: The Call instance which activated the trigger.
+    /// - Parameter glossary: A Glossary instance with information relevant to this Call.
     func performActions(forCall call: Call, glossary: Config.Glossary) {
         self.actions.forEach { action in
             switch action.service {
@@ -40,6 +48,9 @@ extension Rule {
 }
 
 extension Array where Element == Rule {
+    /// Return an array of Rules that match a given Call.
+    ///
+    /// - Parameter call: A Call to test the trigger numbers with.
     func matching(_ call: Call) -> [Rule] {
         return self.filter { rule -> Bool in
             return call.dialedNumber == rule.trigger.phoneNumber
